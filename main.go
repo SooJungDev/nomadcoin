cc549167cb0497e3df3b842af9c2362a61e3eebb
crystal <crystal2840@gmail.com>
2022-01-18 21:55:59 +0900

    EXPLORER 강의듣고 추가

--- main.go
+++ main.go
@@ -1,19 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"github.com/SooJungDev/nomadcoin/blockchain"
-)
+import "github.com/SooJungDev/nomadcoin/explorer"
 
 func main() {
-	chain := blockchain.GetBlockChain()
-	chain.AddBlock("Second Block")
-	chain.AddBlock("Third Block")
-	chain.AddBlock("Fourth Block")
-	for _, block := range chain.AllBlocks() {
-		fmt.Printf("Data %s\n", block.Data)
-		fmt.Printf("Hash %s\n", block.Hash)
-		fmt.Printf("PrevHash %s\n", block.PrevHash)
-		fmt.Println()
-	}
+	explorer.Start()
 }
